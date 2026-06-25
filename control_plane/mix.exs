793b63aeecccf81dd73c09eab4bdde77bb31c34f
Bunk Hosting (automation) <stijnvdpol0@gmail.com>
2026-06-25 12:44:42 +0200

    test(cp): add lazy_html test dep for LiveViewTest DOM assertions

--- control_plane/mix.exs
+++ control_plane/mix.exs
@@ -42,7 +42,9 @@ defmodule ControlPlane.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      # LiveViewTest DOM assertions (phoenix_live_view 1.2+) need an HTML parser.
+      {:lazy_html, ">= 0.1.0", only: :test}
     ]
   end
 
